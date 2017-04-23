b81c4aeddd3ebc293b2275f1ba43f304007afbb5
Fare9 <eduardo.blazquez@edu.uah.es>
2017-04-22 19:27:29 -0500

    Finalizado crawler twitter (estable)

--- modules/twitter_crawlera.py
+++ modules/twitter_crawlera.py
@@ -12,58 +12,148 @@
     User Name: OSINTretasNoPastebin
     User: OSINTPastebin
     Password: ojetedevaca
+
+
+    @author: Fare9
+    @mail: farenain9(at)gmail(dot)com
 '''
 
-# librerías crawling para javascript (selenium)
-import selenium
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-# librerías para procesamiento de html (BeautifulSoup)
-from bs4 import BeautifulSoup
+from __base__ import *
 
 
 # Otras librerías interesantes
 import re
 import sys
 import time
+import urllib
 
-class TwitterCrawler():
+logo = '''
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XX                                                                          XX
+XX   MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMMssssssssssssssssssssssssssMMMMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+XX   MMMMMy''                                                    ''yMMMMM   XX
+XX   MMMy'                                                          'yMMM   XX
+XX   Mh'                                                              'hM   XX
+XX   -                                                                  -   XX
+XX                                                                          XX
+XX   ::                                                                ::   XX
+XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+XX                                oo      oo                                XX
+XX           oM                 oo          oo                 Mo           XX
+XX         oMMo                M              M                oMMo         XX
+XX       +MMMM                 s              s                 MMMM+       XX
+XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
+XX                                                                          XX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-    def __init__(self,time_to_crawl,user,password):
-        
-        self.time_to_crawl = time_to_crawl
+    .o88o.                               o8o                .
+    888 `"                               `"'              .o8
+   o888oo   .oooo.o  .ooooo.   .ooooo.  oooo   .ooooo.  .o888oo oooo    ooo
+    888    d88(  "8 d88' `88b d88' `"Y8 `888  d88' `88b   888    `88.  .8'
+    888    `"Y88b.  888   888 888        888  888ooo888   888     `88..8'
+    888    o.  )88b 888   888 888   .o8  888  888    .o   888 .    `888'
+   o888o   8""888P' `Y8bod8P' `Y8bod8P' o888o `Y8bod8P'   "888"      d8'
+.o...P'
+
+
+Twitter crawler to get pastebin URLs, you can change verbosity and time to crawl
+this crawler is one of the modules for the OSINT tool OSINTretasNoHayPastebines,
+you can use it and modify it.
+
+@author: Fare9
+@mail: farenain9(at)gmail(dot)com
+
+
+Ascii art from: https://github.com/syntax-samurai/fsociety
+'''
+
+class TwitterCrawler(Base):
+
+    def __init__(self,verbosity,time_to_crawl,user,password):
+        '''
+            +   user = nombre de usuario en twitter
+            +   password = password en twitter
+        '''
+        Base.__init__(self,verbosity,time_to_crawl)
         self.user = user
         self.password = password
         self.driver = None
         self.dict_for_tweets = {} # guardaremos el id como la clave, si no tiene ID pues no es tweet
         # los tweets suelen tener un id como stream-item-tweet-854876533420412928
         # al usarlo como clave de diccionario, no se repetirá, guardamos objetos beatufiulsoup 
-        # y otra clase ya los procesará
 
+        self.urls_pastebin = []
         # cadenas para buscar en twitter
-        self.look_for = ["pastebin.com","pastebin.com and anonymous","exploit and pastebin.com"]
+        self.look_for = ["pastebin.com","pastebin.com anonymous","exploit pastebin.com","@illegalFawn pastebin.com"]
 
     def run(self):
         '''
             Ejecuta crawler
         '''
+        print logo
+        
         retorno = self.start_crawler()
         if retorno is None:
-            sys.exit(-1)
+            return None
 
         usIn,pssIn = self.get_login_inputs()
+        if usIn is None or pssIn is None:
+            return None
+
         time.sleep(2)
-        self.login_twitter(usIn,pssIn)
+        retorno = self.login_twitter(usIn,pssIn)
+        if retorno is None:
+            return None
 
         for string in self.look_for:
             # buscamos la cadena obtenemos los tweets y lo metemos en nuestro diccionario
             self.look_for_string(string)
             retorno = self.give_me_that_tweets()
             self.dict_for_tweets.update(retorno)
+
+        # después buscaremos en el contenido las URLs de pastebin
+        self.urls_pastebin = self.get_pastebin_urls(self.dict_for_tweets)
+        self.print_verbosity("[+] URLs PASTEBIN: "+str(self.urls_pastebin),1)
+
+        self.driver.quit()
+        return self.urls_pastebin
 
     def start_crawler(self):
         '''
@@ -77,27 +167,37 @@ class TwitterCrawler():
             time.sleep(5)
             return 0
         except Exception as e:
-            print "[-] ERROR START_CRAWLER: "+str(e)
+            self.print_verbosity("[-] ERROR START_CRAWLER: "+str(e),0)
             return None
 
     def get_login_inputs(self):
         '''
             Dame los inputs ricos para loguearme
         '''
-        userInput = self.driver.find_element_by_xpath("//input[@name='session[username_or_email]']")
-        passwordInput = self.driver.find_element_by_xpath("//input[@name='session[password]']")
-        
-        return userInput,passwordInput
+        try:
+            userInput = self.driver.find_element_by_xpath("//input[@name='session[username_or_email]']")
+            passwordInput = self.driver.find_element_by_xpath("//input[@name='session[password]']")
+            
+            return userInput,passwordInput
+        except Exception as e:
+            self.print_verbosity("[-] ERROR GET_LOGIN_INPUTS: "+str(e),0)
+            return None,None
+
     def login_twitter(self,usIn,pssIn):
         '''
             Finalmente entra en twitter
         '''
-        usIn.click()
-        usIn.send_keys(self.user)
-        pssIn.click()
-        pssIn.send_keys(self.password)
-        pssIn.send_keys(Keys.ENTER)
-        time.sleep(4)
+        try:
+            usIn.click()
+            usIn.send_keys(self.user)
+            pssIn.click()
+            pssIn.send_keys(self.password)
+            pssIn.send_keys(Keys.ENTER)
+            time.sleep(4)
+            return 0
+        except Exception as e:
+            self.print_verbosity("[-] ERROR LOGIN_TWITTER: "+str(e),0)
+            return None
 
     def look_for_string(self,lookfor):
         '''
@@ -108,7 +208,7 @@ class TwitterCrawler():
         query_box.clear()
         query_box.send_keys(lookfor)
         query_box.send_keys(Keys.ENTER)
-        print "[?] Buscando por: "+str(lookfor)
+        self.print_verbosity("[?] Buscando por: "+str(lookfor),1)
         time.sleep(4)
 
     def give_me_that_tweets(self):
@@ -125,12 +225,12 @@ class TwitterCrawler():
         tweetsList = {}
         a = time.time()
 
+        self.print_verbosity("[+] Obteniendo tweets",1)
         while (time.time()-a) < float(self.time_to_crawl*2): #crawlearemos por un tiempo
                                                              #ya que se hace espera de 2 segundo, pues multiplicamos por 2
                                                              #los segundos que entran por los que salen
             bsObject = BeautifulSoup(self.driver.page_source,"html5lib")
             # obtenemos primero el ol por su id
-            print "[+] Obteniendo tweets"
             olTweets = bsObject.find("ol",{"id":"stream-items-id"})
 
             # ahora obtenemos sus li
@@ -142,16 +242,65 @@ class TwitterCrawler():
                 if li.has_attr("id"):
                     # directo al diccionario
                     if li["id"] not in tweetsList:
-                        print "[+] Insertando tweet con id: "+str(li["id"])
+                        self.print_verbosity("[+] Insertando tweet con id: "+str(li["id"]),2)
                         tweetsList[str(li["id"])] = li
 
             # ahora scroll
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(2)
 
-        print "[!] Número total de tweets obtenidos: "+str(len(tweetsList))
+        self.print_verbosity("[!] Número total de tweets obtenidos: "+str(len(tweetsList)),1)
         return tweetsList
 
+    def get_pastebin_urls(self,tweets):
+        '''
+            Obtener las url de pastebin a base de expresión 
+            regular, eso de las regex parecía potentillo
+        '''
+        pastebin_urls = set() # un set, así no se repiten
+        for tweet in tweets:
+            # como es un li, obtenemos el text
+            self.print_verbosity("[+] Buscando URL para el tweet: "+tweet,2)
+            tweet_text = tweets[tweet].find("p").get_text()
+            # para buscar los enlaces normales
+            regex = r"pastebin\.com/[a-zA-Z1-9]{8}"
+            enlaces = re.findall(regex,tweet_text)
+            # para buscar los enlaces raw, y los limpiamos
+            regex = r"pastebin\.com/raw/[a-zA-Z1-9]{8}"
+            enlacesraw = re.findall(regex,tweet_text)
+            for i in range(len(enlacesraw)):
+                enlacesraw[i] = enlacesraw[i].replace("/raw","")
+
+            enlaces += enlacesraw
+
+            self.print_verbosity("[!] Enlaces encontrados: "+str(enlaces),3)
+            for enlace in enlaces:
+                if self.check_url(enlace):
+                    pastebin_urls.add(enlace)
+
+        return pastebin_urls
+
+    def check_url(self,url):
+        '''
+            Método para ver si una URL sigue en pie
+            en ese caso la podemos mandar 
+        '''
+        self.print_verbosity("[+] Checkeando si la URL: "+url+" esta online",3)
+        try:
+            if "https://" not in url:
+                url = "https://"+url
+
+            response_code = urllib.urlopen(url).getcode()
+            if response_code == 200:
+                self.print_verbosity("[+] EXISTE",3)
+                return True
+            else:
+                self.print_verbosity("[+] NO EXISTE",3)
+                return False
+        except Exception as e:
+            self.print_verbosity("[+] NO EXISTE",3)
+            return False
+
 if __name__ == '__main__':
-    tc = TwitterCrawler(20,'OSINTPastebin','ojetedevaca')
+    tc = TwitterCrawler(2,5,'OSINTPastebin','ojetedevaca')
     tc.run()
